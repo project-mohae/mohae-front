84db56a53f7cc852913acdb720130713392f157a
EdenHwang <hgs_0529@naver.com>
2022-06-12 00:24:21 +0900

    FEAT: setting redux 6.12

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { wrapper } from "../store";
+
 import Head from "next/head";
+import wrapper from "../store/configureStore";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
---
+++ store/configureStore.ts
@@ -0,0 +1,20 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { createWrapper } from "next-redux-wrapper";
+import logger from "redux-logger";
+import rootReducer from "../reducers";
+
+const isDev = process.env.NODE_ENV === "development";
+const createStore = () => {
+  const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+    devTools: isDev,
+  });
+  return store;
+};
+
+const wrapper = createWrapper(createStore, {
+  debug: isDev,
+});
+
+export default wrapper;
